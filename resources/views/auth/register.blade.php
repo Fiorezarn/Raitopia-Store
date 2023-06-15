756e19d272584c6af9c66bb8b4e7e3134666a56f
Nama Anda <fioreza1902@gmail.com>
2023-06-15 09:57:55 +0700

    fix: register form style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.app')
-
+@section('css', '/css/register.css')
+@section('title','Sign Up')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
+            <div class="card-body">
+                <div class="judul-register">
+                    <p>REGISTER</p>
+                </div>
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
@@ -26,12 +27,26 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="phonenumber" class="col-md-4 col-form-label text-md-end">{{ __('Phone Number') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="phonenumber" type="number" class="form-control @error('phonenumber') is-invalid @enderror" name="phonenumber" value="{{ old('phonenumber') }}" required autocomplete="phonenumber">
+
+                                @error('phonenumber')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -63,13 +78,14 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="submit-register">
                                     {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
+                        <p class="do-you">Have you an account? <a href="login" class="login-here">
+                             Login here! </a> </p>
                     </form>
-                </div>
             </div>
         </div>
     </div>
